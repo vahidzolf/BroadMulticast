80d9c4ff79104c9451005f5640c2ffd6432e68ce
Edoardo <edoardo.maione@gmail.com>
2018-10-14 11:17:22 +0200

    popolati i set con i tipi di protocolli

--- src/protKinds.py
+++ src/protKinds.py
@@ -1,5 +1,23 @@
 
 pPC:set=['_sleep-proxy', '_sftp-ssh', '_adisk', '_afpovertcp', '_pdl-datastream', '_net-assistant',
          '_device-info', '_ssh', '_workstation', '_teamviewer', '_companion-link', '_smb', '_sleep-proxy', '_rfb',
-         '_nomachine', '_afpovertcp', '_sftp-ssh', '_net-assistant']
+         '_nomachine', '_afpovertcp', '_sftp-ssh', '_net-assistant', '_airdrop', '_sketchmirror', '_distcc', '_eppc',
+         '_esdevice', '_esfileshare', '_hudson', '_ichat', '_jenkins', '_keynotepair', '_omnistate', '_photoshopserver',
+         '_raop', '_telnet', '_tunnel', '_udisks-ssh']
+
+pSERVER:set=['_adisk', '_afpovertcp', '_nfs', '_ssh', '_smb', '_webdavs', '_apple-sasl', '_cloud', '_hudson',
+             '_jenkins', '_readynas', '_servermgr', '_xserveraid']
+
+pPRINTER:set=['_ftp', '_ipps', '_pdl-datastream', '_scanner', '_ipp', '_printer', '_fax-ipp', '_riousbprint',
+              '_ica-networking']
+
+pMEDIA:set=['_spotify-connect', '_airplay', '_amzn-wplay', '_appletv-v2', '_atc', '_daap', '_cloud', '_dpap',
+            '_googlecast', '_hap', '_homekit', '_home-sharing', '_mediaremotetv', '_nvstream', '_raop', '_rsp',
+            '_touch-able']
+
+pMOBILE:set=['_companion-link', '_apple-mobdev2', '_airdroid', '_airdrop', '_KeynoteControl', '_keynotepair',
+             '_touch-able', '_esdevice', '_esfileshare', ]
+
+pACCESSPOINT:set=['_riousbprint', '_airport']
+
 
